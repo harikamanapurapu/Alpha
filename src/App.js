2fc15ce2ce52182f61541cd00ca39b38f0a1aa58
Harika <harikamanapurapu@gmail.com>
2023-09-06 16:00:48 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import AddTask from './Components/AddTask';
 import axios from "axios"
 import "./App.css"
@@ -21,13 +20,13 @@ const App = () => {
 
   const fetchTasks = async () => {
     try {
-      const responseToDo = await axios.get('https://alphansat-api1.onrender.com/api/tasks/To%20Do');
+      const responseToDo = await axios.get('http://localhost:4000/api/tasks/To%20Do');
       setTasksToDo(responseToDo.data);
 
-      const responseDoing = await axios.get('https://alphansat-api1.onrender.com/api/tasks/Doing');
+      const responseDoing = await axios.get('http://localhost:4000/api/tasks/Doing');
       setTasksDoing(responseDoing.data);
 
-      const responseDone = await axios.get('https://alphansat-api1.onrender.com/api/tasks/Done');
+      const responseDone = await axios.get('http://localhost:4000/api/tasks/Done');
       setTasksDone(responseDone.data);
     } catch (error) {
       console.error('Error fetching tasks:', error);
@@ -39,10 +38,10 @@ const App = () => {
   const handleAddTask = async (newTask, status) => {
     try {
       if (editingTask) {
-        await axios.patch(`https://alphansat-api1.onrender.com/api/tasks/${editingTask._id}`, { ...newTask, status });
+        await axios.patch(`http://localhost:4000/api/tasks/${editingTask._id}`, { ...newTask, status });
         setEditingTask(null);
       } else {
-        await axios.post('https://alphansat-api1.onrender.com/api/tasks', { ...newTask, status });
+        await axios.post('http://localhost:4000/api/tasks', { ...newTask, status });
       }
       setShowAddForm(false);
       fetchTasks();
@@ -52,66 +51,6 @@ const App = () => {
   };
 
 
-  const onDragEnd = async (result) => {
-    const { source, destination } = result;
-  
-    if (!destination) return;
-  
-    const sourceTasks = getSourceTasks(source.droppableId);
-    const destTasks = getDestTasks(destination.droppableId);
-  
-    const [draggedTask] = sourceTasks.splice(source.index, 1);
-    destTasks.splice(destination.index, 0, draggedTask);
-  
-    // Update the status of the dragged task
-    const newStatus = destination.droppableId === 'todo' ? 'To Do' : destination.droppableId;
-    draggedTask.status = newStatus;
-  
-    // Update the tasks in the database
-    try {
-      await axios.patch(`https://alphansat-api1.onrender.com/api/tasks/${draggedTask._id}`, {
-        title: draggedTask.title,
-        description: draggedTask.description,
-        status: newStatus,
-      });
-    } catch (error) {
-      console.error('Error updating task status:', error);
-    }
-  
-    // Update the state to reflect the changes
-    setTasksToDo([...tasksToDo]);
-    setTasksDoing([...tasksDoing]);
-    setTasksDone([...tasksDone]);
-  };
-  
-
-  
-
-  const getSourceTasks = (status) => {
-    switch (status) {
-      case 'todo':
-        return tasksToDo;
-      case 'doing':
-        return tasksDoing;
-      case 'done':
-        return tasksDone;
-      default:
-        return [];
-    }
-  };
-
-  const getDestTasks = (status) => {
-    switch (status) {
-      case ' todo':
-        return tasksToDo;
-      case 'doing':
-        return tasksDoing;
-      case 'done':
-        return tasksDone;
-      default:
-        return [];
-    }
-  };
 
   const handleEditTask = task => {
     setEditingTask(task);
@@ -121,7 +60,7 @@ const App = () => {
 
   const handleDeleteTask = async (taskId, status) => {
     try {
-      await axios.delete(`https://alphansat-api1.onrender.com/api/tasks/${taskId}`);
+      await axios.delete(`http://localhost:4000/api/tasks/${taskId}`);
       fetchTasks();
     } catch (error) {
       console.error('Error deleting task:', error);
@@ -138,94 +77,53 @@ const App = () => {
   return (
     <div className="App">
       <h1 className='heading'>Task Manager</h1>
-      <DragDropContext onDragEnd={onDragEnd}>
-      <div className="containers">
-        <Droppable droppableId="todo" >
-          {(provided) => (
-            <div ref={provided.innerRef} {...provided.droppableProps} className="container">
-              <h2 className="container-head">To Do</h2>
-              {tasksToDo.map((task, index) => (
-                <Draggable key={task._id} draggableId={task._id} index={index}>
-                  {(provided) => (
-                    <div
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      className="task"
-                    >
-                      <h3 className='taskTitle'>{task.title}</h3>
-                      <p className='taskDesc'>{task.description}</p>
-                      <div className='box'>
-                        <button className='edit' onClick={() => handleEditTask(task)}>Edit</button>
-                        <button className='delete' onClick={() => handleDeleteTask(task._id, task.status)}>Delete</button>
-                      </div>
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-              <button className="add-button1" onClick={() => handleShowAddForm('To Do')}> + Add Task</button>
+      <div className='containers'>
+        <div className="container">
+          <h2 className='container-head'>To do</h2>
+            {tasksToDo
+          .map(task => (
+            <div className="task" key={task._id}>
+              <h3 className='taskTitle'>{task.title}</h3>
+              <p className='taskDesc'>{task.description}</p>
+              <div  className='box'>
+                <button className='edit' onClick={()=>handleEditTask(task)}>Edit</button>
+                <button className='delete' onClick={() => handleDeleteTask(task._id, task.status)}>Delete</button>
+              </div>
             </div>
-          )}
-        </Droppable>
-        <Droppable droppableId="doing" >
-          {(provided) => (
-            <div ref={provided.innerRef} {...provided.droppableProps} className="container">
-              <h2 className="container-head">Doing</h2>
-              {tasksDoing.map((task, index) => (
-                <Draggable key={task._id} draggableId={task._id} index={index}>
-                  {(provided) => (
-                    <div
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      className="task"
-                    >
-                      <h3 className='taskTitle'>{task.title}</h3>
-                      <p className='taskDesc'>{task.description}</p>
-                      <div className='box'>
-                        <button className='edit' onClick={() => handleEditTask(task)}>Edit</button>
-                        <button className='delete' onClick={() => handleDeleteTask(task._id, task.status)}>Delete</button>
-                      </div>
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-              <button className="add-button2" onClick={() => handleShowAddForm('Doing')}> + Add Task</button>
-            </div>
-          )}
-        </Droppable>
-        <Droppable droppableId="done" >
-          {(provided) => (
-            <div ref={provided.innerRef} {...provided.droppableProps} className="container">
-              <h2 className="container-head">Done</h2>
-              {tasksDone.map((task, index) => (
-                <Draggable key={task._id} draggableId={task._id} index={index}>
-                  {(provided) => (
-                    <div
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      className="task"
-                    >
-                      <h3 className='taskTitle'>{task.title}</h3>
-                      <p className='taskDesc'>{task.description}</p>
-                      <div className='box'>
-                        <button className='edit' onClick={() => handleEditTask(task)}>Edit</button>
-                        <button className='delete' onClick={() => handleDeleteTask(task._id, task.status)}>Delete</button>
-                      </div>
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-              <button className="add-button3" onClick={() => handleShowAddForm('Done')}> + Add Task</button>
-            </div>
-          )}
-        </Droppable> 
+          ))}
+          <button className="add-button1" onClick={() => handleShowAddForm('To Do')}> + Add Task</button> 
         </div>
-    </DragDropContext>
+        <div className="container">
+          <h2 className='container-head'>Doing</h2>
+          {tasksDoing
+          .map((task) => (
+            <div className="task" key={task._id}>
+              <h3 className='taskTitle'>{task.title}</h3>
+              <p className='taskDesc'>{task.description}</p>
+              <div  className='box'>
+                <button className='edit'onClick={() => handleEditTask(task)}>Edit</button>
+                <button className='delete' onClick={() => handleDeleteTask(task._id, task.status)}>Delete</button>
+              </div>
+            </div>
+          ))}
+          <button className="add-button2" onClick={() => handleShowAddForm('Doing')}>+ Add Task</button>
+        </div>
+        <div className="container">
+          <h2 className='container-head'>Done</h2>
+          {tasksDone
+          .map(task => (
+            <div className="task" key={task._id}>
+              <h3 className='taskTitle'>{task.title}</h3>
+              <p className='taskDesc'>{task.description}</p>
+              <div  className='box'>
+                <button className='edit' onClick={() => handleEditTask(task)}>Edit</button>
+                <button className='delete' onClick={() => handleDeleteTask(task._id, task.status)}>Delete</button>
+              </div>
+            </div>
+          ))}
+          <button className="add-button3" onClick={() => handleShowAddForm('Done')} >+ Add Task</button>
+        </div>
+    </div>
     {showAddForm && (<AddTask onAddTask={handleAddTask} setShowAddForm={setShowAddForm} selectedStatus={selectedStatus} editingTask={editingTask}  />)}
     </div>
   );
